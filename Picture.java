daef0ece4e5678f375ad48b61d8f71bf7d2f7561
Joshun <joshua.oleary@btinternet.com>
2014-11-14 01:53:58 +0000

    Add b&w image

--- Picture.java
+++ Picture.java
@@ -9,6 +9,9 @@
 import sheffield.*;
 
 public class Picture {
+	enum colours { BLUE, GREEN_1, GREEN_2, BROWN };
+
+	
 	public static void main(String args[]) {
 		
 		/* Screen size (in pixels) and input filename */
@@ -16,20 +19,25 @@ public class Picture {
 		final int BLOCK_SIZE = 3;
 		final int ARRAY_SIZE = SCREEN_SIZE / BLOCK_SIZE;
 		
+		/* Screen size for black and white image */
+		final int BW_SCREEN_SIZE = 200;
+		
 		final String FILE_INPUT = "picture.txt";
 
-		/* Colour values in R, G, B format */
-		final int[] COLOUR_1 = { 255, 0, 0 };
-		final int[] COLOUR_2 = { 0, 255, 0 };
-		
+		/* Colour values in R, G, B format */		
 		final int[] COLOUR_BLUE = { 0, 0, 255 };
 		final int[] COLOUR_BROWN = { 179, 120, 12 };
 		final int[] COLOUR_GREEN_1 = { 14, 148, 9 };
 		final int[] COLOUR_GREEN_2 = { 10, 252, 42 };
 		
+		final int[] COLOUR_WHITE = { 255, 255, 255 };
+		final int[] COLOUR_BLACK = { 0, 0, 0 };
+		
 		/* Create new objects for datafile and display */
 		EasyReader fileInput = new EasyReader(FILE_INPUT);
-		EasyGraphics display = new EasyGraphics(SCREEN_SIZE, SCREEN_SIZE);		
+		EasyGraphics display = new EasyGraphics(SCREEN_SIZE, SCREEN_SIZE);
+		EasyGraphics bwDisplay = new EasyGraphics(BW_SCREEN_SIZE, BW_SCREEN_SIZE);
+		
 		
 		/* Load file contents into string */
 		final String fileData = fileInput.readString();
@@ -39,22 +47,26 @@ public class Picture {
 		
 		boolean done = false;
 		
+		int currentChar = 0;
+		
 		/* Load values from string into integer array */
 		for(int screenRow=0; screenRow<ARRAY_SIZE && !done; screenRow++) {
 			for(int screenColumn=0; screenColumn<ARRAY_SIZE; screenColumn++) {
-				int currentChar = (screenRow * ARRAY_SIZE) + screenColumn;
+				currentChar = (screenRow * ARRAY_SIZE) + screenColumn;
 				
 				/* Check to make sure there aren't too many numbers for array */
 				if( currentChar > (fileData.length() - 1) ) {
-					System.out.println("Finished loading numbers (found " + (currentChar + 1) + ")");
 					done = true;
 					break;
 				}
 				
 				screenSquares[screenRow][screenColumn] = Character.getNumericValue(fileData.charAt(currentChar));
-				System.out.println(screenSquares[screenRow][screenColumn]);
+				//System.out.println(screenSquares[screenRow][screenColumn]);
+				System.out.println(currentChar);
 			}
 		}
+		System.out.println("Finished loading numbers (found " + (currentChar + 1) + ")");
+
 		/* Display colour values on screen */
 		for(int screenRow=0; screenRow<ARRAY_SIZE; screenRow++) {
 			for(int screenColumn=0; screenColumn<ARRAY_SIZE; screenColumn++) {
@@ -72,12 +84,62 @@ public class Picture {
 				 * Must use {3 + ...} as highest element is 199 (one-less) */
 				int xCoord = SCREEN_SIZE - (3 + screenColumn * 3);
 				int yCoord = SCREEN_SIZE - (3 + screenRow * 3);
-				System.out.println(xCoord + " " +  yCoord);
+				//System.out.println(xCoord + " " +  yCoord);
 				display.fillRectangle(xCoord, yCoord, 3, 3);
 			}
 		}
 		
-	
+		
+		
+		/* Work in progress! */
+		int colour = 0;
+		colours colourType = colours.BROWN;
+		colours rowColourType = colours.BROWN;
+		colours columnColourType = colours.BROWN;
+		
+		colours lastColumnColour = colours.BROWN, lastRowColour = colours.BROWN;
+		boolean isColumnEdge = false;
+		boolean isRowEdge = false;
+		
+		
+		/* Display colour values on screen */
+		for(int screenRow=0; screenRow<ARRAY_SIZE; screenRow++) {
+			for(int screenColumn=0; screenColumn<ARRAY_SIZE; screenColumn++) {
+				colour = screenSquares[screenRow][screenColumn];
+				
+				if( colour >= 0 && colour <= 3 )
+					colourType = colours.BLUE;
+				else if( colour >= 4 && colour <= 5 )
+					colourType = colours.GREEN_1;
+				else if( colour >= 6 && colour <= 7 )
+					colourType = colours.GREEN_2;
+				else
+					colourType = colours.BROWN;
+				
+				if( colourType != lastColumnColour )
+					isColumnEdge = true;
+				else
+					isColumnEdge = false;
+				
+				if( isColumnEdge || isRowEdge ) {
+					bwDisplay.setColor(COLOUR_BLACK[0], COLOUR_BLACK[1], COLOUR_BLACK[2]);
+				}
+				else
+					bwDisplay.setColor(COLOUR_WHITE[0], COLOUR_WHITE[1], COLOUR_WHITE[2]);
+
+				bwDisplay.plot(BW_SCREEN_SIZE - (screenColumn + 1), BW_SCREEN_SIZE - (screenRow + 1));
+				lastColumnColour = colourType;
+			}
+			
+			
+			if( colourType != lastRowColour )
+				isRowEdge = true;
+			else
+				isRowEdge = false;
+
+			
+			lastRowColour = colourType;			
+		}
 		
 	}
 }
